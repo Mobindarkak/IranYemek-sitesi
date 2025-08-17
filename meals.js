97631036fc3eb9bfc95f595bb4589ef265225561
Mobin Darkak <mobindarkak2018@gmail.com>
2025-08-17 22:41:49 +0300

    first commit

---
+++ meals.js
@@ -0,0 +1,31 @@
+// meals.js
+const meals = [
+  {
+    name: "فسنجان (Fesenjan)",
+    image: "img/fesenjan.jpg",
+    description: "Nar ekşisi ve cevizle yapılan, genellikle tavukla servis edilen geleneksel İran yemeği.",
+    author: "Anne usulü",
+    link: "fesenjan.html"
+  },
+  {
+    name: "قورمه سبزی (Ghormeh Sabzi)",
+    image: "img/ghormesabzi.jpg",
+    description: "Fasulye, kuru limon ve yeşil otlarla yapılan İran mutfağının vazgeçilmezi.",
+    author: "Ninemin tarifi",
+    link: "ghormeh.html"
+  },
+  {
+    name: "کباب کوبیده (Kabab Koobideh)",
+    image: "img/kabab.jpg",
+    description: "Kıyma, soğan ve baharatla hazırlanan ve şişte pişirilen meşhur İran kebabı.",
+    author: "Usta kebapçıdan",
+    link: "koobideh.html"
+  },
+  {
+    name: "ته چین (Tahchin)",
+    image: "img/tahchin.jpg",
+    description: "Safran, yoğurt ve tavukla yapılan, fırında pişen altın renkli pilav yemeği.",
+    author: "Safran kokulu",
+    link: "tahchin.html"
+  }
+];
