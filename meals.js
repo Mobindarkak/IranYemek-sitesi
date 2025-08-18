e2307125f17e6137bb60b20ccb44431e74117dfb
Mobin Darkak <mobindarkak2018@gmail.com>
2025-08-18 14:54:09 +0300

    turkce ve farsca dil degisim butonu

--- meals.js
+++ meals.js
@@ -1,31 +1,63 @@
 // meals.js
-const meals = [
+
+const meals_tr = [
   {
-    name: "فسنجان (Fesenjan)",
+    name: "Fesenjan",
     image: "img/fesenjan.jpg",
     description: "Nar ekşisi ve cevizle yapılan, genellikle tavukla servis edilen geleneksel İran yemeği.",
     author: "Anne usulü",
     link: "fesenjan.html"
   },
   {
-    name: "قورمه سبزی (Ghormeh Sabzi)",
+    name: "Ghormeh Sabzi",
     image: "img/ghormesabzi.jpg",
     description: "Fasulye, kuru limon ve yeşil otlarla yapılan İran mutfağının vazgeçilmezi.",
     author: "Ninemin tarifi",
     link: "ghormesabzi.html"
   },
   {
-    name: "کباب کوبیده (Kabab Koobideh)",
+    name: "Kabab Koobideh",
     image: "img/kebab.jpg",
     description: "Kıyma, soğan ve baharatla hazırlanan ve şişte pişirilen meşhur İran kebabı.",
     author: "Usta kebapçıdan",
-    link: "koobideh.html"
+    link: "kebab.html"
   },
   {
-    name: "ته چین (Tahchin)",
+    name: "Tahchin",
     image: "img/tahchin.jpg",
     description: "Safran, yoğurt ve tavukla yapılan, fırında pişen altın renkli pilav yemeği.",
     author: "Safran kokulu",
+    link: "tahchin.html"
+  }
+];
+
+const meals_fa = [
+  {
+    name: "فسنجان",
+    image: "img/fesenjan.jpg",
+    description: "خورشتی سنتی با گردو و رب انار که اغلب با مرغ سرو می‌شود.",
+    author: "مادرانه",
+    link: "fesenjan.html"
+  },
+  {
+    name: "قورمه سبزی",
+    image: "img/ghormesabzi.jpg",
+    description: "خورشتی خوش‌عطر با سبزی‌های معطر، لوبیا و لیمو عمانی.",
+    author: "دستور مادربزرگ",
+    link: "ghormesabzi.html"
+  },
+  {
+    name: "کباب کوبیده",
+    image: "img/kebab.jpg",
+    description: "کباب ایرانی تهیه‌شده از گوشت چرخ‌کرده، پیاز و ادویه‌ها.",
+    author: "کباب‌پز حرفه‌ای",
+    link: "kebab.html"
+  },
+  {
+    name: "ته چین",
+    image: "img/tahchin.jpg",
+    description: "پلو زعفرانی با مرغ که در فر پخته می‌شود و ته‌دیگ طلایی دارد.",
+    author: "زعفران‌دار",
     link: "tahchin.html"
   }
 ];
